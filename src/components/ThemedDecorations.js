8d3fe5bde4cb2df3253f3ea45237b4f7b8b0ddc7
Michael Anderson <michanders@msn.com>
2018-02-28 23:06:12 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -4,8 +4,14 @@ import React from 'react';
 export default class ThemedDecorations extends React.Component {
   render() {
     return(
-      <div>{this.props.children}</div>
-      })
-    )
+      const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      });
+    });
+
+    return (
+      <div>{childrenWithExtraProp}</div>
+    );
   }
-};
+}
