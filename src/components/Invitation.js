72290aa647ccc696c0c79ab45150081ef2d1f9e2
Michael Anderson <michanders@msn.com>
2018-02-28 23:03:05 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,15 @@
 // Code Invitation Component Here
+
+export default class Invitation extends React.Component {
+  render() {
+    return(
+      <h1>"You've been invited!"</h1>
+      React.Children.map(this.props.children, child => {
+        return React.cloneElement(child)
+
+        })
+      })
+
+    )
+  }
+}
